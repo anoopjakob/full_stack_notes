8153705fefbb94d816db54968fc225ca4b86b385
Anoop Jacob <anupjakob@gmail.com>
2020-09-08 13:42:33 +0530

    incorporating jQuery to Websites

---
+++ Web development/jQuery/js/index.js
@@ -0,0 +1,10 @@
+// This does the exact thing as writing the jquery code at before the end of the body
+
+// $(document).ready(function ()){
+//
+//   $("h1").css("color","red");
+//
+// }
+
+
+$("h1").css("color","red");
