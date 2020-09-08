bfa5068dffb2644f4c49a6460b635f10a2068015
Anoop Jacob <anupjakob@gmail.com>
2020-09-08 22:35:22 +0530

    adding event-listeners using jQuery

--- Web development/jQuery/js/index.js
+++ Web development/jQuery/js/index.js
@@ -1,13 +1,4 @@
-// This does the exact thing as writing the jquery code at before the end of the body
-
-// $(document).ready(function ()){
-//
-//   $("h1").css("color","red");
-//
-// }
-
-
-$("h1").addClass("big-title margin-50");
-$("h1").text("bye");
-
-$("button").html("<em>dont click me</em>");
+// $("a").attr("href","https://www.yahoo.com");
+$("h1").click(function (){
+  $("h1").css("color","blue");
+})
